c1ac5b2e6618a34752f1b91aa40db17e463c2c99
Robinson Silva <robinson.silva@piauiconectado.com.br>
2021-08-30 16:29:07 -0400

    NOTIFICAÇÃO DE NOVO POST TEMPO REAL COM LARAVEL

--- resources/js/Echo.js
+++ resources/js/Echo.js
@@ -1,5 +1,9 @@
+import Vue from 'vue'
+
 window.Echo.channel('laravel_database_post-created')
 .listen('PostCreated', (e) => {
     console.log(e)
     console.log(e.post)
+
+    Vue.$vToastify.success(`Título do post ${e.post.title}`, 'Novo Post')
 })
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+.postCss('resources/css/app.css', 'public/css', [
+    //
+])
+.webpackConfig({
+    resolve: {
+        alias: { 'vue$': 'vue/dist/vue.js' }
+    }
+})
