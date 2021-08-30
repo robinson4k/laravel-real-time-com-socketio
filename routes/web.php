ef46c3a3bc55ef0b77f4d24ccdfeab3037533372
Robinson Silva <robinson.silva@piauiconectado.com.br>
2021-08-30 15:28:59 -0400

    CONFIGURANDO BROADCAST NO LARAVEL

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\PostCreated;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
@@ -8,10 +9,12 @@ use Illuminate\Support\Str;
 Route::get('/create-post', function () {
     $user = User::first();
 
-    $user->posts()->create([
+    $post = $user->posts()->create([
         'title' => Str::random(150),
         'body' => Str::random(150),
     ]);
+
+    // event(new PostCreated($post));
 
     return 'ok';
 });
